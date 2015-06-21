682f545eb45efca744fb1173c4f08b3ae7a3f02e
bluewing9 <lld490112728@gmail.com>
2015-06-21 20:39:02 +0800

    new modify

--- static/bigba/widget/selectcar/usecar/usecar.js
+++ static/bigba/widget/selectcar/usecar/usecar.js
@@ -1,37 +1,4 @@
 define('bigba:widget/selectcar/usecar/usecar.js', function(require, exports, module){ var cal = require("bigba:static/libs/plugin/calendar/calendar.js");
-var options = {
-    $AutoPlay: false,                                    //[Optional] Whether to auto play, to enable slideshow, this option must be set to true, default value is false
-    $AutoPlaySteps: 1,                                  //[Optional] Steps to go for each navigation request (this options applys only when slideshow disabled), the default value is 1
-    $AutoPlayInterval: 4000,                            //[Optional] Interval (in milliseconds) to go for next slide since the previous stopped if the slider is auto playing, default value is 3000
-    $PauseOnHover: 1,                               //[Optional] Whether to pause when mouse over if a slider is auto playing, 0 no pause, 1 pause for desktop, 2 pause for touch device, 3 pause for desktop and touch device, 4 freeze for desktop, 8 freeze for touch device, 12 freeze for desktop and touch device, default value is 1
-    $Loop: 0,                                       //[Optional] Enable loop(circular) of carousel or not, 0: stop, 1: loop, 2 rewind, default value is 1
-
-    $ArrowKeyNavigation: true,   			            //[Optional] Allows keyboard (arrow key) navigation or not, default value is false
-    $SlideDuration: 500,                                //[Optional] Specifies default duration (swipe) for slide in milliseconds, default value is 500
-    $MinDragOffsetToSlide: 20,                          //[Optional] Minimum drag offset to trigger slide , default value is 20
-    //$SlideWidth: 600,                                 //[Optional] Width of every slide in pixels, default value is width of 'slides' container
-    //$SlideHeight: 300,                                //[Optional] Height of every slide in pixels, default value is height of 'slides' container
-    $SlideSpacing: 5, 					                //[Optional] Space between each slide in pixels, default value is 0
-    $DisplayPieces: 1,                                  //[Optional] Number of pieces to display (the slideshow would be disabled if the value is set to greater than 1), the default value is 1
-    $ParkingPosition: 0,                                //[Optional] The offset position to park slide (this options applys only when slideshow disabled), default value is 0.
-    $UISearchMode: 1,                                   //[Optional] The way (0 parellel, 1 recursive, default value is 1) to search UI components (slides container, loading screen, navigator container, arrow navigator container, thumbnail navigator container etc).
-    $PlayOrientation: 1,                                //[Optional] Orientation to play slide (for auto play, navigation), 1 horizental, 2 vertical, 5 horizental reverse, 6 vertical reverse, default value is 1
-    $DragOrientation: 3,                                //[Optional] Orientation to drag slide, 0 no drag, 1 horizental, 2 vertical, 3 either, default value is 1 (Note that the $DragOrientation should be the same as $PlayOrientation when $DisplayPieces is greater than 1, or parking position is not 0)
-
-    $ThumbnailNavigatorOptions: {
-        $Class: $JssorThumbnailNavigator$,              //[Required] Class to create thumbnail navigator instance
-        $ChanceToShow: 2,                               //[Required] 0 Never, 1 Mouse Over, 2 Always
-        $Loop: 2,                                       //[Optional] Enable loop(circular) of carousel or not, 0: stop, 1: loop, 2 rewind, default value is 1
-        $AutoCenter: 3,                                 //[Optional] Auto center thumbnail items in the thumbnail navigator container, 0 None, 1 Horizontal, 2 Vertical, 3 Both, default value is 3
-        $Lanes: 1,                                      //[Optional] Specify lanes to arrange thumbnails, default value is 1
-        $SpacingX: 2,                                   //[Optional] Horizontal space between each thumbnail in pixel, default value is 0
-        $SpacingY: 2,                                   //[Optional] Vertical space between each thumbnail in pixel, default value is 0
-        $DisplayPieces: 4,                              //[Optional] Number of pieces to display, default value is 1
-        $ParkingPosition: 0,                            //[Optional] The offset position to park thumbnail
-        $Orientation: 2,                                //[Optional] Orientation to arrange thumbnails, 1 horizental, 2 vertical, default value is 1
-        $DisableDrag: true                             //[Optional] Disable drag or not, default value is false
-    }
-};
 var def_cal_option ={
     onSelectDay:function(date,week){
         console.log(date);
@@ -47,19 +14,69 @@ function initcalendar(){
         input:"back_date"
     },def_cal_option));
 }
-module.exports = {
-    /**
-     * widget js 入口函数
-     */
-    
-    init : function() {
-    	this.$el = $(".widget-selectcar-tabs");
-    	var galleryList = $(".slider_con");
-    	galleryList.each(function(i,ele){
-    		new $JssorSlider$(ele, options);
-    	});
-        initcalendar();
-    }
+function initcarnumber(list){
+    list.each(function(index,ele){
+        $(this).click(function(e){
+            var $par = $(e.currentTarget);
+            var act = $(e.target).attr("data-node");
+            var _input = $par.find("[data-node=input]");
+            var _num = parseInt(_input.val());
+            if(act == "add"){
+                _input.val(++_num);
+            }else if(act == "minus"){
+                if(_num<=0){
+                    _input.val(0);
+                    return;
+                }
+                _input.val(--_num);
+            }
+            //alert($dom.attr("data-node"));
+        });
+    });
 }
+
+module.exports = Widget.extend({
+    el: '#widget-selectcar-usecar',
+    events: {
+        'click [data-node=nextBtn]': 'nextBtnClick'
+    },
+    init : function() {
+        var carNumberList = this.$el.find("[data-node=selectNum]");
+        initcarnumber(carNumberList);
+        this.initcalendar();
+        this.initbacktips();
+    },
+    initcalendar: function(){
+        var me = this;
+        var carWay = me.$el.find("[data-node=carway]");
+        var backLabel = me.$el.find("[data-node=back_date]");
+        carWay.click(function(e){
+            var _val = $(e.target).val();
+            if(_val == 1){
+                backLabel.addClass("disable").find("input,select").attr("disabled",true);
+            }else if(_val ==2){
+                backLabel.removeClass("disable").find("input,select").attr("disabled",false);
+            }
+        });
+    },
+    initbacktips: function(){
+        var me = this;
+        var backtips = me.$el.find("[data-node=blacktips]");
+        var _input = me.$el.find("[data-node=selecttip]");
+        var selectArr = [];
+        backtips.on("click",".blacktip",function(e){
+            var selectArr = [];
+            var _dom = $(e.currentTarget);
+            _dom.toggleClass("select");
+            backtips.find(".select").each(function(index,ele){
+                selectArr.push(ele.text());
+            });
+            _input.val(selectArr.join("-"));
+        });
+    },
+    nextBtnClick: function(){
+        
+    }
+});
  
 });
--- template/bigba/widget/index/tuijian/tuijian.tpl
+++ template/bigba/widget/index/tuijian/tuijian.tpl
@@ -8,7 +8,7 @@
         {%foreach $wdata as $item%}
         <div class="box" data-id="{%$item.car_id%}">
             <div class="bpic">
-                <img src="{%$item.media_path%}" width="192" height="201">
+                <img src="{%$item.media_path%}" width="292" height="201">
             </div>
             <div class="bdesc">
                 <p class="desc1">{%$item.car_title%}</p>
@@ -20,84 +20,6 @@
             </div>
         </div>
         {%/foreach%}
-    	<div class="box">
-    		<div class="bpic">
-    			<img src="/static/bigba/images/bus.png">
-    		</div>
-    		<div class="bdesc">
-    			<p class="desc1">31座  宇通客车企业班车／团队旅行</p>
-    			<p class="desc2">6000元保险，预定立减100元</p>
-    			<div class="btn-desc">
-    				<span class="ora1">¥ 300起</span>
-    				<span class="fr desc2">预定立减</span>
-    			</div>
-    		</div>
-    	</div>
-    	<div class="box">
-    		<div class="bpic">
-    			<img src="/static/bigba/images/bus.png">
-    		</div>
-    		<div class="bdesc">
-    			<p class="desc1">31座  宇通客车企业班车／团队旅行</p>
-    			<p class="desc2">6000元保险，预定立减100元</p>
-    			<div class="btn-desc">
-    				<span class="ora1">¥ 300起</span>
-    				<span class="fr desc2">预定立减</span>
-    			</div>
-    		</div>
-    	</div>
-    	<div class="box">
-    		<div class="bpic">
-    			<img src="/static/bigba/images/bus.png">
-    		</div>
-    		<div class="bdesc">
-    			<p class="desc1">31座  宇通客车企业班车／团队旅行</p>
-    			<p class="desc2">6000元保险，预定立减100元</p>
-    			<div class="btn-desc">
-    				<span class="ora1">¥ 300起</span>
-    				<span class="fr desc2">预定立减</span>
-    			</div>
-    		</div>
-    	</div>
-    	<div class="box">
-    		<div class="bpic">
-    			<img src="/static/bigba/images/bus.png">
-    		</div>
-    		<div class="bdesc">
-    			<p class="desc1">31座  宇通客车企业班车／团队旅行</p>
-    			<p class="desc2">6000元保险，预定立减100元</p>
-    			<div class="btn-desc">
-    				<span class="ora1">¥ 300起</span>
-    				<span class="fr desc2">预定立减</span>
-    			</div>
-    		</div>
-    	</div>
-    	<div class="box">
-    		<div class="bpic">
-    			<img src="/static/bigba/images/bus.png">
-    		</div>
-    		<div class="bdesc">
-    			<p class="desc1">31座  宇通客车企业班车／团队旅行</p>
-    			<p class="desc2">6000元保险，预定立减100元</p>
-    			<div class="btn-desc">
-    				<span class="ora1">¥ 300起</span>
-    				<span class="fr desc2">预定立减</span>
-    			</div>
-    		</div>
-    	</div>
-    	<div class="box">
-    		<div class="bpic">
-    			<img src="/static/bigba/images/bus.png">
-    		</div>
-    		<div class="bdesc">
-    			<p class="desc1">31座  宇通客车企业班车／团队旅行</p>
-    			<p class="desc2">6000元保险，预定立减100元</p>
-    			<div class="btn-desc">
-    				<span class="ora1">¥ 300起</span>
-    				<span class="fr desc2">预定立减</span>
-    			</div>
-    		</div>
-    	</div>
     </div>
     <div class="divider"></div>
 </div>
